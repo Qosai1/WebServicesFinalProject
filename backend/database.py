006f110fab2c8e3c9e0e906a2763a3b1dc7411c2
Qosai1 <qosaibadaha26@gmail.com>
2025-06-11 19:00:49 +0300

    nn

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 
-
 client = MongoClient("mongodb://localhost:27017/")
 
 
@@ -14,6 +13,24 @@ victims_collection = db["victims"]
 risk_collection = db["victim_risk_assessments"]
 case_status_history_collection = db["case_status_history"]
 report_evidence_collection = db["report_evidence"]
+
+
+
+# email = "Qusai@gmail.com"
+# password = "Qusai123"
+# role = "admin"
+
+# users_collection.delete_one({"email": email}) 
+
+# hashed_pw = hash_password(password)
+
+# users_collection.insert_one({
+#     "email": email,
+#     "password": hashed_pw,
+#     "role": role
+# })
+
+# print("Admin added with bcrypt password.")
 
 
 
